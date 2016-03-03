763cb5cb34c93b6d46805bcff3f1177d89353da6
Fouad Wahabi <fouad.wahabi@gmail.com>
2016-03-03 11:43:13 +0100

    Fix mcac middleware

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ var passport = require('passport');
 var MCABackendStrategy = require('bms-mca-token-validation-strategy').MCABackendStrategy;
 
 // Tell passport to use the MCA strategy
-// passport.use(new MCABackendStrategy())
+passport.use(new MCABackendStrategy())
 
 // Tell application to use passport
 app.use(passport.initialize());
